8f30581bac19f3a0ff616006658399972a4a3f2c
AcSsalazar <acsalazar@unal.edu.co>
2025-11-08 20:11:25 -0500

    Typing minimal correction

--- frontend/src/pages/Theory.tsx
+++ frontend/src/pages/Theory.tsx
@@ -1,10 +1,10 @@
-import ManualViewer from "../components/MarkdownViewer"
+import MarkdownViewer from "../components/MarkdownViewer"
 
 
 function TheoryDoc() {
   return (
     <div style={{ padding: '2rem', maxWidth: '900px', margin: '0 auto' }}>
-      <ManualViewer filePath="/BaseTeorica.md" />
+      <MarkdownViewer filePath="/BaseTeorica.md" />
     </div>
   );
 }
